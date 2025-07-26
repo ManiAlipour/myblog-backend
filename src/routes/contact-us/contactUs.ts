a5c58fd7ab01c8c64768f6c4b91425d622e9194d
ManiAlipour <mani.alipour.dev@gmail.com>
2025-07-26 17:32:56 +0330

    Enhance README with detailed project description, features, environment variables, and API routes. Update contact us route to include admin functionalities for message management, improving overall user interaction and administrative control.

--- src/routes/contact-us/contactUs.ts
+++ src/routes/contact-us/contactUs.ts
@@ -2,10 +2,16 @@ import { Router } from "express";
 import { authMiddleware } from "../../middleware/authMiddleware";
 import * as contactUsController from "../../controllers/contact-us/contactUsController";
 import { messageValidation } from "../../validators/contactUsValidator";
+import { adminMiddleware } from "../../middleware/adminMiddleware";
 
 const router = Router();
 
+router.get("/", contactUsController.getMessages);
 router.use(authMiddleware);
 router.post("/", messageValidation, contactUsController.sendMessage);
+
+router.use(adminMiddleware);
+router.put("/", contactUsController.setShowInSatisfactions);
+router.put("/allow", contactUsController.allowMessage);
 
 export default router;
