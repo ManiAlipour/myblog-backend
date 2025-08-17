cf99d250885fb70127b6e87880578b64f82a225b
ManiAlipour <mani.alipour.dev@gmail.com>
2025-08-17 14:15:41 +0330

    `Update MongoDB connection URL in dbConnect.ts`

--- src/utils/services/dbConnect.ts
+++ src/utils/services/dbConnect.ts
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const connectToDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGODB_URI as string);
+    const conn = await mongoose.connect(process.env.DATABASE_URL as string);
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
     console.error("MongoDB connection error:", error);
